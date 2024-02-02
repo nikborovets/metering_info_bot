40f37d830dfc4c831cff6f5ff761726f70c5f5be
kakzhelenb <nborovets@yandex.ru>
2024-02-02 11:38:20 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,127 @@
+import telebot
+from telebot import types
+import sheets_google
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+bot = telebot.TeleBot(os.getenv('TOKEN'))
+
+credentials_file = 'creds.json'
+spreadsheet_id = os.getenv('SPREADSHEET_ID')
+googlesheets_link = os.getenv('GOOGLESHEETS_LINK')
+
+
+questions_list = [
+    "Показания счетчика ХВ",
+    "Показания счетчика ГВ",
+    "Показания счетчика электроэнергии",
+]
+
+user_answers = {}
+user_states = {}
+
+QUESTION_STATE = 'questionstate'
+
+def set_user_state(chat_id, state):
+    user_states[chat_id] = state
+
+def get_user_state(chat_id):
+    return user_states.get(chat_id, None)
+
+def ask_questions(chat_id, message_id, flat_id):
+    set_user_state(chat_id, f'{QUESTION_STATE}_{flat_id}')
+    if flat_id == 'flat1':
+        flat_address = 'Бутлерова'
+    elif flat_id == 'flat2':
+        flat_address = 'Климашкина'
+    bot.send_message(chat_id, f"Начинаем заполнение таблицы для квартиры <b>{flat_address}</b>. Введите:", parse_mode='html')
+    ask_next_question(chat_id, message_id, flat_id, 0)
+
+def ask_next_question(chat_id, message_id, flat_id, question_index):
+    if question_index < len(questions_list):
+        current_question = questions_list[question_index]
+        bot.send_message(chat_id, current_question)
+    else:
+        sent_message = bot.send_message(chat_id=chat_id, text="Показания счетчиков сохранены локально. Спасибо!")
+        print(user_answers[flat_id], flat_id)
+        print(type(user_answers[flat_id]), type(flat_id))
+
+        info = sheets_google.GoogleSheetsHandler(credentials_file, spreadsheet_id).read_data(flat_id, 'A1:E50', 'COLUMNS')
+        # bot.send_message(chat_id, str(info['values']))
+        len_columns = len(info['values'][0])
+        # bot.send_message(chat_id, len_columns)
+
+        start_row = len_columns + 1
+        write_range = f'A{start_row}:C{start_row}'
+        data_list_to_write = [user_answers[flat_id]]
+        # print(type(data_list_to_write))
+
+        sheets_google.GoogleSheetsHandler(credentials_file, spreadsheet_id).write_data(flat_id, write_range, data_list_to_write, 'ROWS')
+        print('строка записана')
+
+        
+        link_text = f"[GoogleSheets]({googlesheets_link})"
+
+        updated_message = f"Показания счетчиков сохранены в {link_text}. Спасибо!"
+        bot.edit_message_text(chat_id=chat_id, message_id=sent_message.message_id, text=updated_message, parse_mode='Markdown')
+
+        user_answers[flat_id] = []
+        set_user_state(chat_id, None)
+
+def check_len_sheets_columns(data):
+    return len(data['values'][0])
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Выбрать квартиру")
+    item2 = types.KeyboardButton("Добавить квартиру")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ помогу с заполнением таблицы квартплаты.".format(message.from_user),
+        parse_mode='html', reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
+def handle_messages(message):
+    chat_id = message.chat.id
+    message_id = message.message_id
+    current_state = get_user_state(chat_id)
+
+    if message.text == "Выбрать квартиру":
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        item1 = types.InlineKeyboardButton("Бутлерова", callback_data='flat1')
+        item2 = types.InlineKeyboardButton("Климашкина", callback_data='flat2')
+        markup.add(item1, item2)
+        bot.send_message(chat_id, 'Выбери квартиру из списка', reply_markup=markup)
+    elif current_state and current_state.startswith(QUESTION_STATE):
+        flat_id = current_state.split('_')[1]
+        current_question_index = len(user_answers.get(flat_id, []))
+        user_answer = message.text
+
+        if flat_id not in user_answers:
+            user_answers[flat_id] = []
+
+        user_answers[flat_id].append(user_answer)
+        next_question_index = current_question_index + 1
+
+        ask_next_question(chat_id, message_id, flat_id, next_question_index)
+    else:
+        bot.send_message(chat_id, "Это обработка вне состояния вопросов.")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    chat_id = call.message.chat.id
+    message_id = call.message.message_id
+    if call.message:
+        if call.data == 'flat1':
+            # info = sheets_google.GoogleSheetsHandler(credentials_file, spreadsheet_id).read_data('flat1', 'A1:E10', 'COLUMNS')
+            # bot.send_message(call.message.chat.id, str(info['values']))
+            # bot.send_message(call.message.chat.id, str(len(info['values'][0])))
+            set_user_state(chat_id, QUESTION_STATE)
+            ask_questions(chat_id, message_id, 'flat1')
+        elif call.data == 'flat2':
+            set_user_state(chat_id, QUESTION_STATE)
+            ask_questions(chat_id, message_id, 'flat2')
+
+bot.polling(none_stop=True)
---
+++ sheets_google.py
@@ -0,0 +1,86 @@
+from pprint import pprint
+import httplib2
+import googleapiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+from typing import List, Any
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
+
+credentials_file = 'creds.json'
+spreadsheet_id = os.getenv('SPREADSHEET_ID')
+googlesheets_link = os.getenv('GOOGLESHEETS_LINK')
+
+class GoogleSheetsHandler:
+    def __init__(self, credentials_file, spreadsheet_id):
+        self.credentials_file = credentials_file
+        self.spreadsheet_id = spreadsheet_id
+        self.service = self.authorize()
+
+    def authorize(self):
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            self.credentials_file,
+            ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
+        )
+        http_auth = credentials.authorize(httplib2.Http())
+        return googleapiclient.discovery.build('sheets', 'v4', http=http_auth)
+
+    def read_data(self, sheet_name, range_name, majorDimension):
+        full_range = f'{sheet_name}!{range_name}'
+        # majorDimension = 'ROWS'
+        values = self.service.spreadsheets().values().get(
+            spreadsheetId=self.spreadsheet_id,
+            range=full_range,
+            majorDimension=majorDimension
+        ).execute()
+        return values
+
+    def write_data(self, sheet_name: str, range_name: str, data_list: List[List[Any]], majorDimension: str):
+        full_range = f'{sheet_name}!{range_name}'
+        start_row = int(range_name.split(':')[0][1:])
+        body = {
+            "valueInputOption": "USER_ENTERED",
+            "data": [
+                {
+                    "range": full_range,
+                    "majorDimension": majorDimension,
+                    "values": data_list,
+                },
+            ]
+        }
+        values = self.service.spreadsheets().values().batchUpdate(
+            spreadsheetId=self.spreadsheet_id,
+            body=body
+        ).execute()
+
+if __name__ == "__main__":
+
+    sheets_handler = GoogleSheetsHandler(credentials_file, spreadsheet_id)
+
+
+    sheet_name = 'flat1'
+
+
+    read_range = 'A1:E30'
+    read_values = sheets_handler.read_data(sheet_name, read_range, 'ROWS')
+    pprint(read_values)
+
+
+    data_list_to_write = [['1', 'John Doe', '500', '20.02.2023'],
+                          ['2', 'Jane Smith', '600', '21.02.2023'],
+                          ['3', 'Ivan Ivanov', '700', '22.02.2023']]
+    
+    # start_row = 1
+    # write_range = f'A{start_row}:D{start_row + len(data_list_to_write)}'
+
+
+    # sheets_handler.write_data(sheet_name, write_range, data_list_to_write)
+
+
+    # data_list_to_write1 = [['твтвдв', 'твтвьдва', 'воовлу']]
+    # len_columns = 5
+    # start_row = len_columns + 1
+    # write_range = f'A{start_row}:C{start_row}'
+    # sheets_handler.write_data('flat1', write_range, data_list_to_write1, 'ROWS')
+    # print(type(data_list_to_write1))
+
